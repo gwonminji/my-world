5cc178607647962d8fdb4224eb25bcd68e8ef978
gwonminji <gmj@kipis.co.kr>
2023-06-26 14:31:12 +0900

    sass 설치

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,9 @@
+import styles from "../scss/header.module.scss"
 export default function Header(){
     return(
-        <header>header</header>
+        <header className={styles.header}>
+            <div className={styles.logo}>logo</div>
+            <button className={styles.btn}>button</button>
+        </header>
     )
 }
