1b480212e3fab9da1489aae91d8f18fb777bd12f
gwonminji <gmj@kipis.co.kr>
2023-07-03 10:32:02 +0900

    section3 리스트 애니메이션 적용

--- src/components/home/Section3.js
+++ src/components/home/Section3.js
@@ -1,7 +1,66 @@
+import {
+    motion
+} from "framer-motion";
+
+import styles from "@styles/components/home/section3.module.scss";
+
 export default function Section3(){
+    const list = {
+        hidden: {
+            opacity: 0
+        },
+        visible: {
+            opacity: 1,
+            transition: {
+                type: "spring",
+                stiffness: 60,
+                // delayChildren: 0.1,
+                when: "beforeChildren",
+                staggerChildren: 0.2
+            }
+        }
+    }
+    const item = {
+        hidden: { 
+            opacity: 0,
+            y: 50 
+        },
+        visible: { 
+            opacity: 1,
+            y: 0 
+        }
+    }
+
     return(
         <div>
-            section3
+            <motion.ul 
+                className={styles.list}
+                variants={list}
+                initial="hidden"
+                whileInView="visible"
+            >
+                <motion.li 
+                    className={styles.item}
+                    variants={item}
+                >
+                    <div className={styles.box}>box1</div>
+                    <div className={styles['text-box']}>text1</div>
+                </motion.li>
+                <motion.li 
+                    className={styles.item}
+                    variants={item}
+                >
+                    <div className={styles.box}>box2</div>
+                    <div className={styles['text-box']}>text2</div>
+                </motion.li>
+                <motion.li 
+                    className={styles.item}
+                    variants={item}
+                >
+                    <div className={styles.box}>box3</div>
+                    <div className={styles['text-box']}>text3</div>
+                </motion.li>
+            </motion.ul>
         </div>
     )
 }
