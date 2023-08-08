ce2d7dc43e4d52efac0ef2fb2f902ce2b92b82bc
gwonminji <gmj@kipis.co.kr>
2023-08-08 15:02:53 +0900

    header 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,15 @@ import logo from '@assets/images/logo.png';
 export default function Header(){
     return(
         <header className={styles.header}>
-            <div className={styles.inner}>
-                <h1 className={styles.logo}>
-                    {/* <img src={logo} alt="" /> */}
-                    <img src="" alt="Hello" />
-                </h1>
+            {/* <div className={styles.inner}> */}
+                <a href="/">
+                    <h1 className={styles.logo}>
+                        {/* <img src={logo} alt="" /> */}
+                        <img src="" alt="Hello" />
+                    </h1>
+                </a>
                 {/* <button className={styles.btn}>button</button> */}
-            </div>
+            {/* </div> */}
         </header>
     )
 }
