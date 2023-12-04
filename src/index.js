b38880ae81b4d2fcdae9758c58cc94b35ed73852
gwonminji <blueelf1124@gmail.com>
2023-12-05 03:47:15 +0900

    404 제거

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import ScrollToTop from "@components/ScrollToTop";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter>
     <ScrollToTop />
     <App />
   </BrowserRouter>
